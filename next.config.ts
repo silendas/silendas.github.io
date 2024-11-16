1ba0f8821bb6fd4b7fcc6894176118afad3dc2e1
silendas <muhammadyasminnulhakim34@gmail.com>
2024-11-16 16:18:35 +0700

    Fix deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: '/silendas',
   images: {
     unoptimized: true
   },
-  trailingSlash: true,
+  trailingSlash: false,
 };
 
 export default nextConfig;
